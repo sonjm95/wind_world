059b95d93bf0e556debd881166f912f393aa7e64
somjm95 <sonjmo95@gmail.com>
2020-08-13 15:01:43 +0900

    프로젝트 시작

---
+++ ww_back/src/main/java/com/wind_world/back/WwBackApplication.java
@@ -0,0 +1,13 @@
+package com.wind_world.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwBackApplication.class, args);
+	}
+
+}
